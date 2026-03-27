5fda349ee7c8861e5f0b3164c4c2d375b0818244
fayaz unar <ifayazunar@gmail.com>
2026-03-27 09:28:59 +0500

    Add files via upload

---
+++ enter a string.py
@@ -0,0 +1,2 @@
+user_input=(input("enter a string:"))
+print("you entered: ",user_input)                       
---
+++ single character.py
@@ -0,0 +1,5 @@
+char_input=input("enter single character:")
+if len(char_input)==1:
+    print("you anter the character: ",char_input)
+else:
+    print("please enter only one character")
